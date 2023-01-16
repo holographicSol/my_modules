340bce3be5a4c71b05f07f50daada8e9916d4e84
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-16 01:49:37 +0000

    Add files via upload

--- power_lists.py
+++ power_lists.py
@@ -29,13 +29,10 @@ def power_repr(*args) -> list:
 
 def power_ord(*args) -> list:
     """ accepts *unpacked list """
-    v = []
-    [v.append(ord(arg)) for arg in args]
-    return v
+    return [ord(arg) for arg in args]
 
 
 def power_chr(*args) -> list:
     """ accepts *unpacked list """
-    v = []
-    [v.append(chr(arg)) for arg in args]
-    return v
+    return [chr(arg) for arg in args]
+
